34e95e1dfafa3c4a3983a69817526d0d0a11d07b
Mahdi <a.b.mahdey@gmail.com>
2020-09-05 22:34:47 +0200

    creating toggling calsses

--- connectedfour.js
+++ connectedfour.js
@@ -1,3 +1,12 @@
 $(".playerOne").eq(0).text(prompt("first player name:"))
 $(".playerTwo").eq(0).text(prompt("second player name:"))
 
+var select=0
+$("td").click(function(){
+    if(select%2==0)
+    $(this).toggleClass("playerTwoTurn")
+    else{
+        $(this).toggleClass("playerOneTurn")
+    }
+    select++
+})
