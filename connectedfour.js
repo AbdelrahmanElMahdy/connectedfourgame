47fa58a5cd92af1c851e7003a896c35f6a525d25
Mahdi <a.b.mahdey@gmail.com>
2020-09-12 21:11:54 +0200

    scrolling balls down algorithm

--- connectedfour.js
+++ connectedfour.js
@@ -1,12 +1,20 @@
 $(".playerOne").eq(0).text(prompt("first player name:"))
 $(".playerTwo").eq(0).text(prompt("second player name:"))
 
-var select=0
+var rows=$("tr").eq(0).prevObject;
+var level=[4,4,4,4,4,4,4,4];
+
+var playerOne=true;
 $("td").click(function(){
-    if(select%2==0)
-    $(this).toggleClass("playerTwoTurn")
-    else{
-        $(this).toggleClass("playerOneTurn")
+    var selected =this.cellIndex;
+    console.log(selected) ;
+    if(playerOne){
+        rows[level[selected]--].children[selected].className="playerOneTurn";
+        playerOne=false;
     }
-    select++
+    else{
+        rows[level[selected]--].children[selected].className="playerTwoTurn";
+        playerOne=true;
+    }
+    selected++;
 })
