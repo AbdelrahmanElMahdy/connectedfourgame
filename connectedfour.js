ee7550d1fd3f97539c5a7eeef63e819e444cfb13
Mahdi <a.b.mahdey@gmail.com>
2020-09-13 20:44:33 +0200

    getting current element's neighbours calsseName

--- connectedfour.js
+++ connectedfour.js
@@ -7,14 +7,81 @@ var level=[4,4,4,4,4,4,4,4];
 var playerOne=true;
 $("td").click(function(){
     var selected =this.cellIndex;
-    console.log(selected) ;
+    actualElement=rows[level[selected]--].children[selected];
     if(playerOne){
-        rows[level[selected]--].children[selected].className="playerOneTurn";
+        actualElement.className="playerOneTurn";
         playerOne=false;
     }
     else{
-        rows[level[selected]--].children[selected].className="playerTwoTurn";
+        actualElement.className="playerTwoTurn";
         playerOne=true;
     }
     selected++;
 })
+
+function checkWinner(element){
+    var temp_element=temp_element2=temp_element3=element
+    var topElementsClasses=topElementsArray(temp_element)
+    var bottomelementCalsses=bottomElementsArray(temp_element2)
+    var rightelementCalsses=rightElementsArray(temp_element3)
+    var leftelementCalsses=leftElementsArray(element)
+    return [topElementsClasses,bottomelementCalsses,rightelementCalsses,leftelementCalsses]
+}
+
+function leftElementsArray(element){
+    var previousElements=[];
+    var columnsCount=0;
+        while(element!==null && columnsCount++ !==4){
+          try {
+            previousElements.push(element.className);
+            element=element.previousElementSibling; 
+          } catch (error) {
+          break;    
+          }  
+            
+        }
+        return previousElements;
+}
+function rightElementsArray(element){
+    var columnsCount=0;
+    var nextElements=[];
+        while(element!==null && columnsCount++ !==4){
+          try {
+            nextElements.push(element.className);
+            element=element.nextElementSibling; 
+          } catch (error) {
+          break;    
+          }  
+            
+        }
+        
+        return nextElements;
+}
+function topElementsArray(element){
+    var topElements=[];
+    var columnsCount=0;
+        while(element!==null && columnsCount++ !==4){
+          try {
+            topElements.push(element.className);
+            element=element.parentNode.previousElementSibling.cells[element.cellIndex];
+          } catch (error) {
+          break;    
+          }  
+            
+        }
+        return topElements;    
+}
+function bottomElementsArray(element){
+    var bottomElements=[];
+    var columnsCount=0;
+        while(element!==null && columnsCount++ !==4){
+          try {
+            bottomElements.push(element.className);
+            element=element.parentNode.nextElementSibling.cells[element.cellIndex];
+          } catch (error) {
+          break;    
+          }  
+            
+        }
+        return bottomElements;    
+}
